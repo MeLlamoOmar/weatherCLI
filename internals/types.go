7ab3ae226b91085d76e489e6187875f60cea42d0
Omar Bencosme Tio <obencosmetio@gmail.com>
2025-11-17 13:04:33 -0400

    Refactor estructura de tipos de clima para mejorar la legibilidad y reutilización

--- internals/types.go
+++ internals/types.go
@@ -1,27 +1,36 @@
 package internals
 
 type Weather struct {
-	Location struct {
-		Name    string `json:"name"`
-		Country string `json:"country"`
-		LocalTime int64 `json:"localtime_epoch"`
-	} `json:"location"`
-	Current struct {
-		TempC     float64 `json:"temp_c"`
-		Condition struct {
-			Text string `json:"text"`
-		} `json:"condition"`
-	} `json:"current"`
-	Forecast struct {
-		ForecastDay []struct {
-			Hour []struct {
-				TimeEpoch int64   `json:"time_epoch"`
-				TempC     float64 `json:"temp_c"`
-				Condition struct {
-					Text string `json:"text"`
-				} `json:"condition"`
-				ChanceOfRain float64 `json:"chance_of_rain"`
-			} `json:"hour"`
-		} `json:"forecastday"`
-	}
+	Location WeatherLocation `json:"location"`
+	Current  WeatherCurrent  `json:"current"`
+	Forecast Forecast `json:"forecast"`
+}
+
+type Forecast struct {
+	ForecastDay []ForecastDay `json:"forecastday"`
+}
+
+type ForecastDay struct {
+	Hour []WeatherForecastHour `json:"hour"`
+}
+
+type WeatherLocation struct {
+	Name    string `json:"name"`
+	Country string `json:"country"`
+}
+
+type WeatherCondition struct {
+	Text string `json:"text"`
+}
+
+type WeatherCurrent struct {
+	TempC     float64 `json:"temp_c"`
+	Condition WeatherCondition `json:"condition"`
+}
+
+type WeatherForecastHour struct {
+	TimeEpoch int64   `json:"time_epoch"`
+	TempC     float64 `json:"temp_c"`
+	Condition WeatherCondition `json:"condition"`
+	ChanceOfRain float64 `json:"chance_of_rain"`
 }
